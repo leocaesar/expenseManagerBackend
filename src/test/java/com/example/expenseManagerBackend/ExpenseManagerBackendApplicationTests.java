00053f3bb2718eb584ece3db35b078aec2cc5348
leo <leocaesar98@gmail.com>
2022-11-02 17:33:43 +0700

    Spring Boot Init

---
+++ src/test/java/com/example/expenseManagerBackend/ExpenseManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseManagerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
